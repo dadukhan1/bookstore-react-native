3727a11937b1b2d72d980f8b68f0fea917c0c33a
Dadu Khan <photoshopt65@gmail.com>
2025-04-15 16:24:31 +0500

    RootLayout setup

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,14 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
     >
+      <Link href="/(auth)">Login</Link>
+      <Link href="/(auth)/signup">Signup</Link>
       <Text>Let him cock</Text>
     </View>
   );
