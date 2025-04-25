001ab3e9a077755772392aeb2411b7ecf8521d7d
Dadu Khan <photoshopt65@gmail.com>
2025-04-25 19:58:35 +0500

    completed

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -42,7 +42,7 @@ route.get("/", protectRoute, async (req, res) => {
     try {
 
         const page = req.query.page || 1;
-        const limit = req.query.limit || 5;
+        const limit = req.query.limit || 2;
         const skip = (page - 1) * limit;
 
         const books = await Book.find()
